1fc9d17f899a0a3464b5af93a54847f84c596f72
Beat Besmer <beat.besmer@gmail.com>
2019-07-05 12:01:01 +0200

    Add quote model

---
+++ db/migrate/20190705091520_create_quotes.rb
@@ -0,0 +1,11 @@
+class CreateQuotes < ActiveRecord::Migration
+  def change
+    create_table :quotes do |t|
+      t.date :date
+      t.string :quote
+      t.string :author
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190705091520) do
+
+  create_table "quotes", force: :cascade do |t|
+    t.date     "date"
+    t.string   "quote",      limit: 255
+    t.string   "author",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
